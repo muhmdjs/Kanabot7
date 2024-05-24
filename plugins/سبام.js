0ab66a9a617dd53369d6033e6b81bcf8c0824766
Kana782 <166362795+Kana782@users.noreply.github.com>
2024-05-24 23:39:06 +0300

    Update سبام.js

--- plugins/سبام.js
+++ plugins/سبام.js
@@ -14,7 +14,7 @@ const handler = async (m, {conn, text}) => {
 
   const fixedNumber = nomor.replace(/[-+<>@]/g, '').replace(/ +/g, '').replace(/^[0]/g, '62') + '@s.whatsapp.net';
   const fixedJumlah = jumlah ? jumlah * 1 : 10;
-  if (fixedJumlah > 50) throw tradutor.texto4;
+  if (fixedJumlah > 350) throw tradutor.texto4;
   await m.reply(`${tradutor.texto5[0]} ${nomor} ${tradutor.texto5[1]}  ${fixedJumlah} ${tradutor.texto5[2]}`);
   for (let i = fixedJumlah; i > 1; i--) {
     if (i !== 0) conn.reply(fixedNumber, pesan.trim(), m);
