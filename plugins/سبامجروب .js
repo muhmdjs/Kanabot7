50951a66d348e9d4b2c7244b0409eed311000096
Kana782 <166362795+Kana782@users.noreply.github.com>
2024-05-24 18:38:08 +0300

    Create سبامجروب .js

---
+++ plugins/سبامجروب .js
@@ -0,0 +1,27 @@
+const handler = async (m, {conn, text}) => {
+  const datas = global;
+  const idioma = datas.db.data.users[m.sender].language;
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`));
+  const tradutor = _translate.plugins.herramientas_spamwa;
+
+  const [pesan, jumlah] = text.split('|');
+  if (!pesan) throw tradutor.texto1;
+  if (jumlah && isNaN(jumlah)) throw tradutor.texto2;
+
+  const fixedJumlah = jumlah ? jumlah * 1 : 10;
+  if (fixedJumlah > 50) throw tradutor.texto3;
+
+  await m.reply(`${tradutor.texto4[0]} ${fixedJumlah} ${tradutor.texto4[1]}`);
+  for (let i = fixedJumlah; i > 0; i--) {
+    conn.reply(m.chat, pesan.trim(), m);
+  }
+};
+
+handler.help = ['spamgroup <message>|<no of messages>'];
+handler.tags = ['General'];
+handler.command = /^سبامجروب$/i;
+handler.group = true;
+handler.premium = true;
+// handler.private = true
+// handler.limit = true
+export default handler;
