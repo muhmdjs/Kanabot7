ebdf9c9377a0ac21aa6bea32c2cc98c40143aadb
Kana782 <166362795+Kana782@users.noreply.github.com>
2024-05-25 14:02:41 +0300

    Update سبامجروب .js

--- plugins/سبامجروب .js
+++ plugins/سبامجروب .js
@@ -1,6 +1,3 @@
-const fs = require('fs');
-const { setTimeout } = require('timers/promises');
-
 const handler = async (m, {conn, text}) => {
   const datas = global;
   const idioma = datas.db.data.users[m.sender].language;
@@ -11,14 +8,12 @@ const handler = async (m, {conn, text}) => {
   if (!pesan) throw tradutor.texto1;
   if (jumlah && isNaN(jumlah)) throw tradutor.texto2;
 
-  const fixedJumlah = jumlah ? jumlah * 1 : 250;
-  if (fixedJumlah > 250) throw tradutor.texto3;
+  const fixedJumlah = jumlah ? jumlah * 1 : 10;
+  if (fixedJumlah > 350) throw tradutor.texto3;
 
   await m.reply(`${tradutor.texto4[0]} ${fixedJumlah} ${tradutor.texto4[1]}`);
-
-  for (let i = 0; i < fixedJumlah; i++) {
+  for (let i = fixedJumlah; i > 0; i--) {
     conn.reply(m.chat, pesan.trim(), m);
-    await setTimeout(100);  // تأخير 100 مللي ثانية بين كل رسالة
   }
 };
 
@@ -26,6 +21,7 @@ handler.help = ['spamgroup <message>|<no of messages>'];
 handler.tags = ['General'];
 handler.command = /^سبامجروب$/i;
 handler.group = true;
+handler.premium = true;
 // handler.private = true
 // handler.limit = true
 export default handler;
