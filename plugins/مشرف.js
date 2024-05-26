553be3ff2064d6f1506d1486c34a222e15b549d9
Kana782 <166362795+Kana782@users.noreply.github.com>
2024-05-26 03:36:21 +0300

    Update مشرف.js

--- plugins/مشرف.js
+++ plugins/مشرف.js
@@ -1,14 +1,23 @@
 let handler = async (m, { conn }) => {
     let txt = ''
+    let isBotAdmin = async (jid) => {
+        try {
+            let participants = (await conn.groupMetadata(jid)).participants
+            let botParticipant = participants.find(participant => participant.id === conn.user.jid)
+            return botParticipant && botParticipant.admin
+        } catch (e) {
+            return false
+        }
+    }
+
     for (let [jid, chat] of Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats)) {
-        let metadata = await conn.groupMetadata(jid)
-        if (metadata.participants.find(participant => participant.jid === conn.user.jid && participant.admin)) {
+        if (await isBotAdmin(jid)) {
             txt += `\n—◉ ${await conn.getName(jid)}\n➤ ${jid} [${chat?.metadata?.read_only ? 'لا يوجد مشاركون' : 'مشارك'}]\n\n`
         }
     }
-    m.reply(`*قائمة الجروبات التي البوت مشرف فيها:*
-${txt}
-`.trim())
+
+    if (txt === '') txt = 'لا توجد مجموعات يكون فيها البوت مشرفًا.'
+    m.reply(`*قائمة الجروبات التي البوت مشرف فيها:*\n${txt}`.trim())
 }
 
 handler.help = ['groups', 'grouplist']
